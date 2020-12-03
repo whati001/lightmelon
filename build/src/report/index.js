415b147cbdcd7fba086b1b0fac6fd18e201ae4cc
whati001 <andreas.karner@student.tugraz.at>
2020-12-03 11:27:23 +0100

    AK - cleanup repo

--- build/src/report/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const RepDriver_1 = __importDefault(require("./RepDriver"));
-const RepWorker_1 = __importDefault(require("./RepWorker"));
-exports.default = { RepDriver: RepDriver_1.default, RepWorker: RepWorker_1.default };
